1cdbc5a59e75161c077bd14eb06a961024765cc3
Nir Ellor <nirellorwaizner@gmail.com>
2026-05-09 20:35:31 +0300

    EPOCHS       = 1000 LR           = 2e-3

--- step0_train_autoencoder.py
+++ step0_train_autoencoder.py
@@ -23,7 +23,7 @@ from models.autoencoder import ConvAutoencoder
 LATENT_DIMS  = [64, 128, 256, 384, 512, 1024]
 EPOCHS       = 1000
 BATCH_SIZE   = 128
-LR           = 1e-3
+LR           = 2e-3
 WEIGHT_DECAY = 0
 GRAD_CLIP    = 5.0
 LPIPS_WEIGHT = 0.1
