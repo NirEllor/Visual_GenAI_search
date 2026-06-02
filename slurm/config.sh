b26dc2626cc095d07dec21b37a24debb30873cc4
Nir Ellor <nirellorwaizner@gmail.com>
2026-06-02 14:05:29 +0300

    NOT 250000

--- slurm/config.sh
+++ slurm/config.sh
@@ -10,4 +10,4 @@ TORCH_CACHE="/cs/labs/raananf/ellorw.nir/torch_cache"
 RUN="source /cs/labs/raananf/ellorw.nir/venv/bin/activate && cd $PROJECT &&"
 
 DIMS=(64 128 256 384 512 1024)
-SIZES=(50000 100000 150000 200000 250000)
+SIZES=(50000 100000 150000 200000)
